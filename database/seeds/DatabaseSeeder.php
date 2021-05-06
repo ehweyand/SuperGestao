b9f65142d01d9fef247c0ea3af0732faffdb7d43
Evandro Henrique <ehweyand@universo.univates.br>
2021-05-05 21:12:53 -0300

    env file and seeds configured

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //$this->call(UsersTableSeeder::class);
         $this->call(FornecedorSeeder::class);
-        $this->call(SiteContatoSeeder::class);
+        //$this->call(SiteContatoSeeder::class);
         $this->call(MotivoContatoSeeder::class);
     }
 }
